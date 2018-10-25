3240745385c9b0f21657856c76f5915189c4d589
MonDc <44021944+MonDc@users.noreply.github.com>
2018-10-25 19:08:55 +0200

    re

---
+++ js/javascritp.js
@@ -0,0 +1,20 @@
+function lightOn(){
+
+document.getElementById("lightImage").src="./imgs/lampOn.png";
+}
+
+function lightOff(){
+
+    document.getElementById("lightImage").src="./imgs/lampOff.png";
+    }
+
+
+//   ----------->>>>> parameters
+    // function lightOn(){
+        //  changeLight("on");
+        //  function changlelight(lightset){
+    // document.getElementById("lightImage").src="./imgs "+ lightset+".png";
+    //
+// }  
+
+    
